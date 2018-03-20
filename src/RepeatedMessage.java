8cdd19054fbbf03457f0280855596807b0e1984a
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-03-19 20:54:01 -0500

    Created methods and input them
    Also completed bones credit

---
+++ src/RepeatedMessage.java
@@ -0,0 +1,14 @@
+
+public class RepeatedMessage {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 1000; i++) {
+		repeatedMessage();
+		}
+	}
+		public static void repeatedMessage() {
+			
+				System.out.println("Milhouse did not test cootie positive");
+			
+		}
+}
--- src/StarFigures.java
+++ src/StarFigures.java
@@ -5,12 +5,50 @@
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
-
+/*
+ *   drawDoubleBars
+ *   drawX
+ *   
+ *   drawDoubleBars
+ *   drawX
+ *   drawDoubleBars
+ *   
+ *   drawSingleStar
+ *   drawSingleStar
+ *   drawSingleStar
+ *   drawDoubleBar
+ *   drawX
+ */
 
 public class StarFigures {
 
 	public static void main(String[] args) {
-
+		drawDoubleBars();
+		drawX();
+		System.out.println(" ");
+		System.out.println(" ");
+		drawDoubleBars();
+		drawX();
+		drawDoubleBars();
+		System.out.println(" ");
+		System.out.println(" ");
+		drawSingleStar();
+		drawSingleStar();
+		drawSingleStar();
+		drawDoubleBars();
+		drawX();
+		
 	}
-
+		public static void drawDoubleBars() {
+			System.out.println("*****");
+			System.out.println("*****");
+		}
+		public static void drawX() {
+			System.out.println(" * * ");
+			System.out.println("  *  ");
+			System.out.println(" * * ");
+		}
+		public static void drawSingleStar() {
+			System.out.println("  *  ");
+		}
 }
