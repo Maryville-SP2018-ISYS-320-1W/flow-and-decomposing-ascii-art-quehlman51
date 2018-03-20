75d6deac7ad7f518d64d1e11a9373435082cb1cf
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-03-19 20:37:43 -0500

    Made predictions, entered data

--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,39 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): A.J. Quehl
+  	Date: 19 Mar 2018
 */
 
 // 1. Your predicted output 
+/*  This is message1.
+ * 	This is message2.
+ * 	This is message1.
+ * 	Done with message2.
+ *  Done with main.
+ */
+
  
 
 public class Tricky {
-
+	public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    
+    public static void message1() {
+        System.out.println("This is message1.");
+    }
+    
+    public static void message2() {
+        System.out.println("This is message2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
 }
 
+
 // 3. Were you correct? Explain any differences
- 
+//    I was correct. 
  
 
